0d2393c1238914e4eb526f264de5a17721468cd3
Inken Wohlers <Inken.Wohlers@uni-luebeck.de>
2019-09-17 07:55:32 +0200

    Added another plot (boxplot) for AF differences.

--- scripts/plot_tag_af_diff.R
+++ scripts/plot_tag_af_diff.R
@@ -5,6 +5,7 @@ fname_in <- snakemake@input[[1]]
 fname_out_hist <- snakemake@output[[1]]
 fname_out_scatter <- snakemake@output[[2]]
 fname_out_missing <- snakemake@output[[3]]
+fname_out_boxplot_afdiff <- snakemake@output[[4]]
 
 data <- read.table(fname_in, header=T)
 dim(data)
@@ -29,6 +30,14 @@ abline(a=-0.2,b=1,col=blue,lwd=5)
 abline(a=0.3,b=1,col=red,lwd=5)
 abline(a=-0.3,b=1,col=blue,lwd=5)
 points(data$EUR_ALT_AF,data$EGP_ALT_AF,pch=21,cex=0.5,col="grey40",bg="grey90")
+dev.off()
+
+pdf(fname_out_boxplot_afdiff,width=5, height=7)
+data_wo_missing <- data[data$EGP_ALT_AF!=0,]
+c1 <- rainbow(10)[4]
+c2 <- rainbow(10, alpha=0.2)[4]
+c3 <- rainbow(10, v=0.7)[4]
+boxplot(data_wo_missing$EUR_ALT_AF-data_wo_missing$EGP_ALT_AF,ylab="European AF - Egyptian AF",cex.axis=0.9,outcex=0.5,col=c2,medcol=c3,whiskcol=c1,staplecol=c3,boxcol=c3,outcol=c2,pch=19)
 dev.off()
 
 pdf(fname_out_missing)
